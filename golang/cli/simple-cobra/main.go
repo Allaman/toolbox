01ce8f1886311518f12ab9e2772ef6ed0d6aec25
Michael <michaeljohannpeter@gmail.com>
2021-12-29 22:43:25 +0100

    simple-cobra add completion

--- golang/cli/simple-cobra/main.go
+++ golang/cli/simple-cobra/main.go
@@ -19,10 +19,32 @@ func main() {
 	vertUpCmd.MarkFlagRequired("memory")
 	rootCmd.AddCommand(statusCmd)
 	rootCmd.AddCommand(vertUpCmd)
+	rootCmd.AddCommand(CompletionCmd)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+var CompletionCmd = &cobra.Command{
+	Use:                   "completion [bash|zsh|fish|powershell]",
+	Short:                 "Generate completion script",
+	Long:                  "To load completions",
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+		}
+	},
 }
 
 var rootCmd = &cobra.Command{
